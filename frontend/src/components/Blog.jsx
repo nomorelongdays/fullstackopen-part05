3603b720bcc9cb9a2999063cba29bcaeca461630
nomorelongdays <jason.kemp@bt.com>
2025-03-25 13:43:35 +0000

    exercise-5.7

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+  const blogStyle = {
+    paddingTop: 3,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const invisible = { display: 'none' }
+  return (
+    <>
+    <div className='blog' style={{...blogStyle, display: show ? 'none' : ''}}>
+      <a href={blog.url}>{blog.title} | {blog.author}</a> <button onClick={() => (setShow(true))}>view</button>
+    </div>  
+    <div className='blog' style={{...blogStyle, display: !show ? 'none' : ''}} >
+    {blog.title}<br />{blog.author}<br />{blog.url}<br />{blog.likes} likes <button onClick={() => (setShow(false))}>hide</button>
   </div>  
-)
+</>  
 
+)}
 export default Blog
