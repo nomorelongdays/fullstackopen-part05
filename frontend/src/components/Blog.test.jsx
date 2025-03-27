eb5734209e9ff863155edfd0a0eface125607dbb
nomorelongdays <jason.kemp@bt.com>
2025-03-27 20:47:31 +0000

    exercise-5.14

--- frontend/src/components/Blog.test.jsx
+++ frontend/src/components/Blog.test.jsx
@@ -38,3 +38,40 @@ test('The component displaying a blog renders the blog\'s title and author, but 
 
 })
 
+//5.14: Blog List Tests, step 2
+//Make a test, which checks that the blog's URL and number of likes are shown when the button controlling the shown details has been clicked.
+ 
+test('the blog\'s URL and number of likes are shown when the button controlling the shown details has been clicked.', async () => {
+  const blog = {
+    title: 'Test Title',
+    author: 'Test Author\'s name',
+    url: 'http://nowhere.com',
+    likes: 5,
+  }
+
+  const { container } = render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const div = container.querySelector('.detail')
+
+  expect(div).toHaveTextContent(
+    'Test Title', { exact: false }
+  )
+
+  expect(div).toHaveTextContent(
+    'Test Author\'s name', { exact: false }
+  )
+
+  expect(div).toHaveTextContent(
+    'http://nowhere.com', { exact: false }
+  )
+ 
+  const form = container.querySelector('.likes')
+  expect(form).toHaveTextContent(
+    '5 likes', { exact: false }
+  )
+
+})
